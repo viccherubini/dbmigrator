7fd7bb8964ebb6289f170e2a08ae6b6e9191cf62
Vic Cherubini <vmc@leftnode.com>
2010-08-24 15:03:28 -0500

    Adding the templated configuration file, copy this file to DbMigrator.Config.php to use system wide.

---
+++ DbMigrator.Config.Template.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(encoding='UTF-8');
+
+define('DB_TYPE', strtoupper('MYSQL'));
+define('DB_HOST', '');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
+
+define('MIGRATION_PATH', '');
